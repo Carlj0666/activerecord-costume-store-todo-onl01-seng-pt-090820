ce7a760e3c6cf6ad57f9455e4d847c4266c36857
Carlj0666 <carlj1881@yahoo.com>
2020-11-18 16:36:54 -0800

    commit tree b98ac1d46a64c759bacd317a440a0b33ecd680bc

--- db/migrate/002_create_costume_stores.rb
+++ db/migrate/002_create_costume_stores.rb
@@ -1,1 +1,17 @@
 # Create your costume_stores migration here
+
+class CreateCostumeStores < ActiveRecord::Migration[5.2]
+  
+  def change
+    create_table :costume_stores do |t|
+      t.string :name
+      t.string :location
+      t.integer :number_of_costumes
+      t.integer :number_of_employees
+      t.string :still_open?
+      t.integer :opening_time
+      t.integer :closing_time
+    end
+  end
+
+end
--- db/migrate/003_create_haunted_houses.rb
+++ db/migrate/003_create_haunted_houses.rb
@@ -1,1 +1,20 @@
 # Create your haunted_houses migration here
+
+
+
+class CreateHauntedHouses < ActiveRecord::Migration[5.2]
+  
+  def change
+    create_table :haunted_houses do |t|
+      t.string :name
+      t.string :location
+      t.string :theme
+      t.integer :price
+      t.string :family_friendly?
+      t.integer :opening_date
+      t.integer :closing_date
+      t.string :description
+    end
+  end
+
+end
